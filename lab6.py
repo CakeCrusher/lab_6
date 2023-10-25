0e56e2e910b8c7c7161176bb72faa964219f0c9c
dev-par <146295367+dev-par@users.noreply.github.com>
2023-10-25 18:23:25 -0400

    updated decode function

--- lab6.py
+++ lab6.py
@@ -1,9 +1,19 @@
+# Sebastian Sosa
+
+
 def encode(password: str):
     # increase the value of each digit in the password by 3
     newPass = ''
     for i in range(len(password)):
         newPass += str(int(password[i]) + 3)
     return newPass
+
+
+def decode(encoded_password):
+    origPass = " "
+    for i in range(len(encoded_password)):
+        origPass += str((int(encoded_password[i]) - 3))
+    return origPass
 
 def main():
     encoded_password = ''
@@ -20,8 +30,9 @@ Please enter an option:""")
             encoded_password = encode(password)
             print("Your password has been encoded and stored!\n")
         elif inp == "2":
-            # CONTRIBUTOR PLEASE ADD DECODER HERE
-            pass
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}")
+
         elif inp == "3":
             break
 
