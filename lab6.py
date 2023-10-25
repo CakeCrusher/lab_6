a49eff5c0521ac3968835489b26b9142c8a7d642
CakeCrusher <1sebastian1sosa1@gmail.com>
2023-10-25 17:54:54 -0400

    string format

--- lab6.py
+++ lab6.py
@@ -1,9 +1,3 @@
-# def decode(password: str):
-#     # decrease the value of each digit in the password by 3
-#     newPass = ''
-#     for i in range(len(password)):
-#         newPass += str(int(password[i]) - 3)
-#     return newPass
 def encode(password: str):
     # increase the value of each digit in the password by 3
     newPass = ''
@@ -12,15 +6,24 @@ def encode(password: str):
     return newPass
 
 def main():
+    encoded_password = ''
     while True:
-        inp = input("Enter a number 0 for decode 1 for encode: ")
-        if inp == "0":
-            # CONTRIBUTOR PLEASE ADD DECODER
-            # password = input("Enter a password: ")
-            # print(decode(password))
-        elif inp == "1":
-            password = input("Enter a password: ")
-            print(encode(password))
+        inp = input("""Menu
+-------------
+1. Encode
+2. Decode
+3. Quit
+
+Please enter an option:""")
+        if inp == "1":
+            password = input("Please enter your password to encode:")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!\n")
+        elif inp == "2":
+            # CONTRIBUTOR PLEASE ADD DECODER HERE
+            pass
+        elif inp == "3":
+            break
 
 if __name__ == "__main__":
     main()
